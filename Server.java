836738fe9bd372bb9dd31c6594be5cb08d4ca948
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2021-08-05 17:12:41 +0100

    Update Server.java
    Refactoring

--- Server.java
+++ Server.java
@@ -61,44 +61,36 @@ class Server {
 			return thread1;
 		}
 		return thread2;
-
 	}
 
 	// creates a thread for the first client to be connected.
-	private void manageFirstClientConnected(Socket firstClientSocket)
-			throws IOException {
+	private void manageFirstClientConnected(Socket firstClientSocket) throws IOException {
 		thread1 = new ServerClientListenerThread(firstClientSocket, 1);
 		new Thread(thread1, "First server thread listener").start();
 
 	}
 
 	// creates a thread for the second client to be connected.
-	private void manageSecondClientConnected(Socket secondClientSocket)
-			throws IOException {
+	private void manageSecondClientConnected(Socket secondClientSocket) throws IOException {
 		thread2 = new ServerClientListenerThread(secondClientSocket, 2);
 		new Thread(thread2, "Second server thread listener").start();
 	}
 
 	// called by client thread that receives an "end" notification from
 	// the connected client to tell the other thread to end
-	public void notifyClosingThread(ServerClientListenerThread clientClosing)
-			throws IOException {
-		otherClient(clientClosing)
-				.sendMessageToClient("other_client_disonnected");
+	public void notifyClosingThread(ServerClientListenerThread clientClosing) throws IOException {
+		otherClient(clientClosing).sendMessageToClient("other_client_disonnected");
 		cleanup();
-
 	}
 
-	public ServerClientListenerThread otherClient(
-			ServerClientListenerThread otherThanThisThread) {
+	public ServerClientListenerThread otherClient(ServerClientListenerThread otherThanThisThread) {
 		if (thread1 == otherThanThisThread) {
 			return thread2;
 		}
 		return thread1;
 	}
 
-	public ServerClientListenerThread thisClient(
-			ServerClientListenerThread thisOne) {
+	public ServerClientListenerThread thisClient(ServerClientListenerThread thisOne) {
 		if (thread1 != thisOne) {
 			return thread1;
 		}
