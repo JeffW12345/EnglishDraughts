1be9b78fbca04933ffda14e53f7411e0b7c4bd67
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2021-08-08 17:26:50 +0100

    Update RunServerAndClients.java

--- RunServerAndClients.java
+++ RunServerAndClients.java
@@ -1,5 +1,6 @@
-// Launches the server and two clients locally. Used for testing and debugging. 
+// Launches the server and two clients locally. Used for testing and debugging - not submitted as part of the project.
 // Creates three threads. First creates a server thread, then creates a thread for each client. 
+// Does the equivalent of typing javac *.java, followed by java Server, followed by java Client twice (in separate browsers). 
 
 import java.io.IOException;
 
