58565902d5b6fd8c0bb9951e528e0226bba99077
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2021-08-08 17:36:00 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,6 +5,6 @@
 public class Client {
 	public static void main(String[] args) {
 		var client = new ClientThread();
-		client.start("Client ref: " + (int) (10000 * Math.random()));
+		client.start("Client debug ref: " + (int) (10000 * Math.random()));
 	}
 }
